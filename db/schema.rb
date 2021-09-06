46a387ff1bfc5b7654a7149d384e0fa478a6c333
Patrycja Miekina <patmiekina@gmail.com>
2021-09-06 20:38:06 +0100

    added username, coordinates, address to user, coordinates to locations & began to write seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_165918) do
+ActiveRecord::Schema.define(version: 2021_09_06_193531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,8 @@ ActiveRecord::Schema.define(version: 2021_09_05_165918) do
     t.bigint "owner_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["owner_id"], name: "index_locations_on_owner_id"
   end
 
@@ -90,6 +92,10 @@ ActiveRecord::Schema.define(version: 2021_09_05_165918) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating 20 users..."
+
+puts "Users created!"
+
+puts "Creating 20 locations..."
+
+puts "Locations created!"
+
+puts "Creating 20 events..."
+
+puts "Events created!"
+
+puts "Creating 10 groups..."
+
+puts "Groups created!"
+
+puts "Creating 20 invites..."
+
+puts "Invites created!"
